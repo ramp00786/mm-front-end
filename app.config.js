196dd4ffa1fb737a653b7b361419c25dd5b09494
Tulsiram Kushwah <ramp00786@gmail.com>
2023-09-22 16:35:50 +0530

    server

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //API_URL_Local: 'https://monsoon-mission.tropmet.res.in/dbtl1dt',
-    API_URL_Local: 'http://127.0.0.1:8000',
+    API_URL_Local: 'https://monsoon-mission.tropmet.res.in/dbtl1dt',
+    //API_URL_Local: 'http://localhost/tulsiram_work/MM-Nextjs-Laravel/back-end/dbtl1dt/public',
 }
